db15fcea975a3d27a2f35d9c722ce746ffc5d31c
Adrian Kreher <avuserow@gmail.com>
2014-04-21 22:43:35 -0700

    Organize functions in Snappy.pm

--- lib/Compress/Snappy.pm
+++ lib/Compress/Snappy.pm
@@ -15,6 +15,27 @@ sub snappy_uncompress(CArray[uint8], Int, CArray[uint8], CArray[int]) returns In
 
 sub snappy_validate_compressed_buffer(CArray[uint8], Int) returns Int is native('libsnappy') {...}
 
+# helper functions to hide translations between Perl and C representations
+sub _zero_array(Int $count) {
+	my $array = CArray[uint8].new();
+	$array[$_] = 0 for ^$count;
+	return $array;
+}
+
+# these two can go away if/when NativeCall learns to translate Blobs to CArrays
+sub _copy_blob_to_array(Blob $blob) {
+	my $array = CArray[uint8].new();
+	$array[$_] = $blob[$_] for ^$blob.bytes;
+	return $array;
+}
+
+sub _int_pointer(Int $value = 0) {
+	# Simulate an int pointer with a CArray
+	my $intpointer = CArray[int].new();
+	$intpointer[0] = $value;
+	return $intpointer;
+}
+
 our sub validate(Blob $blob) returns Bool {
 	my $compressed = _copy_blob_to_array($blob);
 	my $status = snappy_validate_compressed_buffer($compressed, $blob.bytes);
@@ -41,27 +62,6 @@ multi compress(Blob $blob) returns Buf {
 
 multi compress(Str $str) returns Buf {
 	return compress($str.encode("utf8"));
-}
-
-# helper functions to hide translations between Perl and C representations
-sub _zero_array(Int $count) {
-	my $array = CArray[uint8].new();
-	$array[$_] = 0 for ^$count;
-	return $array;
-}
-
-# these two can go away if/when NativeCall learns to translate Blobs to CArrays
-sub _copy_blob_to_array(Blob $blob) {
-	my $array = CArray[uint8].new();
-	$array[$_] = $blob[$_] for ^$blob.bytes;
-	return $array;
-}
-
-sub _int_pointer(Int $value = 0) {
-	# Simulate an int pointer with a CArray
-	my $intpointer = CArray[int].new();
-	$intpointer[0] = $value;
-	return $intpointer;
 }
 
 our sub decompress(Blob $blob) returns Buf {
